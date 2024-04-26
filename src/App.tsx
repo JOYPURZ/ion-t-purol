a7f73f629cc00df6d395fe634001d1851ccf027e
JOYPURZ <20211782@nbsc.edu.ph>
2024-04-26 22:36:24 +0800

    added todolist

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import ClickCounter from './pages/ClickCounter';
 import Calculator from './pages/Calculator';
+import Todolist from './pages/Todolist';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -49,6 +50,9 @@ const App: React.FC = () => (
           <Route exact path="/calculator">
             <Calculator />
           </Route>
+          <Route exact path="/todolist">
+            <Todolist />
+          </Route>
 
 
           <Route exact path="/">
@@ -57,6 +61,7 @@ const App: React.FC = () => (
           <Route exact path="/home">
             <Home />
           </Route>
+
           <Route exact path="/profile">
             <Profile />
           </Route>
